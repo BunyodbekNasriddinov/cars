4bdeca29cea6b872c9aa5dbb105f7c6b9ea61444
Bunyodbek <93827769+BunyodbekNasriddinov@users.noreply.github.com>
2023-07-03 18:38:44 +0500

    add: errors

---
+++ src/utils/errors.ts
@@ -0,0 +1,58 @@
+export class AuthorizationError extends Error {
+  status: number
+  name: string
+  message: string
+  constructor(message: string) {
+    super()
+    this.status = 401
+    this.name = "AuthorizationError"
+    this.message = message
+  }
+}
+export class NotFoundError extends Error {
+  status: number
+  name: string
+  message: string
+  constructor(message: string) {
+    super()
+    this.status = 404
+    this.name = "NotFoundError"
+    this.message = message
+  }
+}
+
+export class BadRequestError extends Error {
+  status: number
+  name: string
+  message: string
+  constructor(message: string) {
+    super()
+    this.status = 400
+    this.name = "BadRequestError"
+    this.message = message
+  }
+}
+
+export class ForbiddenError extends Error {
+  status: number
+  name: string
+  message: string
+  constructor(message: string) {
+    super()
+    this.status = 403
+    this.name = "ForbiddenError"
+    this.message = message
+  }
+}
+
+export class InternalServerError extends Error {
+  status: number
+  name: string
+  message: string
+  constructor(message: string = "InternalServerError") {
+    super()
+    this.status = 500
+    this.name = "InternalServerError"
+    this.message = message
+  }
+}
